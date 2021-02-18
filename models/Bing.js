77bb72ba40012e6b5b8e2f7bb4d4d68ca5973b67
knight-lightning <dba.knight@gmail.com>
2021-02-18 11:50:49 +0300

    swaped to google search

--- models/Bing.js
+++ models/Bing.js
@@ -3,13 +3,13 @@ class BingPage {
         this.page = page
     }
     async navigate() {
-        await this.page.goto('https://www.bing.com/') //Переходим в поисковик
+        await this.page.goto('https://www.google.com/') //Переходим в поисковик
     }
     async searchAndFollow(request) {
-        await this.page.fill('#sb_form_q', request) // Вбиваем наш поисковый запрос
+        await this.page.fill('input', request) // Вбиваем наш поисковый запрос
         await this.page.keyboard.press('Enter') //Жмём Enter
 
-        await this.page.click('"crtweb.ru - Аутсорсинг веб и ..."') // Переходим на искомую страницу
+        await this.page.click('"https://crtweb.ru"') // Переходим на искомую страницу
     }
 }
 
