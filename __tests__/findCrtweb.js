6f439e4ed42abd1d299da707f313b830c05890f3
knight-lightning <dba.knight@gmail.com>
2021-02-18 12:12:06 +0300

    small fix

--- __tests__/findCrtweb.js
+++ __tests__/findCrtweb.js
@@ -4,9 +4,9 @@ const { MainPage } = require('../models/Bing')
 
 describe('Find crtweb.ru, follow contacts', () => {
     test('should display correct contacts', async () => {
-        for (const browserType of ['chromium']) {
+        for (const browserType of ['chromium', 'firefox', 'webkit']) {
             const browser = await playwright[browserType].launch({
-                headless: true, slowMo: 250
+                headless: false, slowMo: 250
             })
             const context = await browser.newContext({
                 viewport: { width: 1920, height: 1080 }
@@ -18,16 +18,9 @@ describe('Find crtweb.ru, follow contacts', () => {
 
             await bingPage.navigate()
             await bingPage.searchAndFollow('crtweb.ru')
-
-            // const page1 = await page.waitForEvent('popup')
-
-            const [page1] = await Promise.all([
-                page.waitForEvent('popup'),
-                page.click('text=/.*crtweb\.ru - Аутсорсинг веб и \..*/')
-            ])
-
+            await page.waitForTimeout(2000)
             // Проверяем, что у нас есть заголовок - мы на нужной странице
-            const content = await page1.textContent('text="Надежные, быстрые, как свои "')
+            const content = await page.textContent('text="Надежные, быстрые, как свои "')
             expect(content).toBe('Надежные, быстрые, как свои ')
 
             // Пример под page object для заполнения формы
@@ -35,23 +28,21 @@ describe('Find crtweb.ru, follow contacts', () => {
             // await mainPage.sendForm()
 
             // Ждём  2 сек и делаем скрин
-            await page1.waitForTimeout(2000)
-            await page1.screenshot({ path: `screens/MainPage-${browserType}.png`, fullPage: true })
+            await page.waitForTimeout(2000)
+            await page.screenshot({ path: `screens/MainPage-${browserType}.png`})
 
             // Переходим по клику на страницу контактов
-            await page1.click('"Контакты"')
+            await page.click('"Контакты"')
 
             // Проверяем корректность телефона
-            const phone = await page1.textContent('"+7 (499) 113-68-89"')
+            const phone = await page.textContent('"+7 (499) 113-68-89"')
             expect(phone).toBe('+7 (499) 113-68-89')
 
             // Проверяем корректность почты
-            const mail = await page1.textContent('"mail@crtweb.ru"')
+            const mail = await page.textContent('"mail@crtweb.ru"')
             expect(mail).toBe('mail@crtweb.ru')
 
-            await page1.screenshot({ path: `screens/ContactPage-${browserType}.png` })
-
-            await page1.close()
+            await page.screenshot({ path: `screens/ContactPage-${browserType}.png` })
             await page.close()
             await context.close()
             await browser.close()
